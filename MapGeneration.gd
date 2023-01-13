a8dc99c87432c8a9304b9f540fa66af4179c18a5
voie420 <66523399+voie420@users.noreply.github.com>
2023-01-13 10:50:41 +0100

    + More Textures + Added Random Colors + New Tileset

--- MapGeneration.gd
+++ MapGeneration.gd
@@ -11,31 +11,41 @@ var GeneratingCell = Vector2(0,0)
 var Generating = false
 
 enum {
-	Part = 0,
+	Purple_Part = 0,
+	Red_Part = 1,
+	Orange_Part = 2,
+	Green_Part = 3,
+	Blue_Part = 4,
 }
+
+var RandomColor = randi()%5
 
 func CreateStartPlatform(x,y):
 	CurrentPlacingPositionX += 1
-	set_cell(x,y,Part)
+	set_cell(x,y,RandomColor)
 	
 	if CurrentPlacingPositionX == StartPlatformPieces:
 		GeneratingCell = Vector2(x,y)
 		CurrentPlacingPositionX = 0
 		
 func CreatePart(x,y):
-	set_cell(x,y,Part)
+	set_cell(x,y,RandomColor)
 	
 func CreatePlatform(length,x,y):
 	for i in length: 
-		set_cell(x + CurrentPlacingPositionX,y,Part)
+		set_cell(x + CurrentPlacingPositionX,y,RandomColor)
 		CurrentPlacingPositionX += 1
 	
 func _physics_process(delta):
 	if Generating:
 		var PlacingOffsetX = 4
 		var PlacingOffsetY = randi()%4
-		CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
+		var Selection = randi()%2
 		
+		if Selection == 0:
+			CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
+		else:
+			CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y + PlacingOffsetY)
 		GeneratingCell = Vector2(GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
 
 func _ready():
