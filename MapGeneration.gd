af2801e37db44455929286de30cca9e7382924b3
cuzcuff <christianellingsen2@gmail.com>
2023-01-16 09:28:13 +0100

    Revert "Merge branch 'main' into cuzcuff"
    This reverts commit 2e8e6b2fe45bfc84a846a7a85a645c9b75b8304f, reversing
    changes made to edb3396dc5de1619b28508313a84b13fef1a87b1.

--- MapGeneration.gd
+++ MapGeneration.gd
@@ -9,48 +9,36 @@ var CurrentPlacingPositionY = 0
 
 var GeneratingCell = Vector2(0,0)
 var Generating = false
-var SelectedColor
 
 enum {
-	Purple_Part = 0,
-	Red_Part = 1,
-	Orange_Part = 2,
-	Green_Part = 3,
-	Blue_Part = 4,
+	Part = 0,
 }
 
 func CreateStartPlatform(x,y):
 	CurrentPlacingPositionX += 1
-	set_cell(x,y,SelectedColor)
+	set_cell(x,y,Part)
 	
 	if CurrentPlacingPositionX == StartPlatformPieces:
 		GeneratingCell = Vector2(x,y)
 		CurrentPlacingPositionX = 0
 		
 func CreatePart(x,y):
-	set_cell(x,y,SelectedColor)
+	set_cell(x,y,Part)
 	
 func CreatePlatform(length,x,y):
 	for i in length: 
-		set_cell(x + CurrentPlacingPositionX,y,SelectedColor)
+		set_cell(x + CurrentPlacingPositionX,y,Part)
 		CurrentPlacingPositionX += 1
 	
 func _physics_process(delta):
 	if Generating:
 		var PlacingOffsetX = 4
-		var PlacingOffsetY = randi()%5
-		var SelectionY = randi()%2
+		var PlacingOffsetY = randi()%4
+		CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
 		
-		if SelectionY == 0:
-			CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
-			GeneratingCell = Vector2(GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
-		else:
-			CreatePlatform(randi()%9,GeneratingCell.x + PlacingOffsetX, GeneratingCell.y + PlacingOffsetY)
-			GeneratingCell = Vector2(GeneratingCell.x + PlacingOffsetX, GeneratingCell.y + PlacingOffsetY)
+		GeneratingCell = Vector2(GeneratingCell.x + PlacingOffsetX, GeneratingCell.y - PlacingOffsetY)
 
 func _ready():
-	randomize()
-	SelectedColor = randi()%5
 	Generating = true
 	for Part in StartPlatformPieces:
 		CreateStartPlatform(CurrentPlacingPositionX, StartPlatformPosition)
