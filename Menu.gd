ed56c7f5dff5f4af65aa9258d62b68ecbaa632a2
Evv3n <eivind.andreas.hans1@gmail.com>
2023-03-06 08:44:24 +0100

    ingenting

--- Menu.gd
+++ Menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	$VBoxContainer/StartButton
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://test.tscn")
+	get_tree().change_scene("res://main.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
